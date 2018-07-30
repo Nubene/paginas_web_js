c82d97b59929f7032ab7294d73aec40c3944f872
nubene <garciaargudo@yahoo.com>
2018-07-30 13:58:32 +0200

    scroll

---
+++ 13_Web_Eventos/Eventos/app.js
@@ -0,0 +1,14 @@
+function.main () {
+
+    document.body.addEventListener
+    ('mousemove', ()=> {
+
+
+    })
+
+
+}
+
+
+//document.addEventListener('DONCOntentLoad',main)
+window.addEventListener('load',main)
